f952c3400c955ac5fddb680fc3b54d2fd23e03ab
Jimmy Morentin <44115002+JimmyMorentin@users.noreply.github.com>
2019-05-21 12:55:57 -0700

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -6,11 +6,15 @@ using namespace std;
 const int ROWS = 9;
 const int COLS = 9;
 
-void playSudoku(int array[ROWS][COLS]);
+void playSudoku(int sudokuArray[ROWS][COLS]);
 
-void crossHatchMarker(int array[ROWS][COLS], int num);
+void crossHatchMarker(int sudokuArray[ROWS][COLS], int num);
 
-void crossHatchPlacer(int array[ROWS][COLS], int sudokuArray[ROWS][COLS], int num);
+void crossHatchPlacer(int parArray[ROWS][COLS], int sudokuArray[ROWS][COLS], int num, int xpos, int ypos);
+
+void subGridAnalyzer(int parArray[ROWS][COLS], int sudokuArray[ROWS][COLS], int num, int xLower, int xUpper, int yLower, int yUpper, int xpos, int ypos);
+
+void placeNum(int parArray[ROWS][COLS], int sudokuArray[ROWS][COLS], int num, int xpos, int ypos);
 
 int main()
 {
@@ -23,7 +27,7 @@ int main()
 		{4, 0, 0, 8, 0, 3, 0, 0, 1},
 		{7, 0, 0, 0, 2, 0, 0, 0, 6},
 		{0, 6, 0, 0, 0, 0, 2, 8, 4},
-		{2, 7, 8, 4, 1, 9, 0, 0, 5},
+		{2, 0, 7, 4, 1, 9, 0, 0, 5},
 		{3, 4, 5, 0, 8, 0, 0, 7, 9}
 	};
 
@@ -45,8 +49,43 @@ int main()
 	system("pause");
 	return 0;
 }
+// REAL FUNCTION
+/*void playSudoku(int sudokuArray[ROWS][COLS])
+{
+	
+	bool flag = false; // Prove puzzle is complete
 
-void playSudoku(int array[ROWS][COLS])
+	while (!flag) // While there are still blank spaces, cross hatch!
+	{
+		int counter = 0; // TAG
+		for (int x = 0; x < ROWS; x++) // Check for empty spaces, represented by a zero
+		{
+			for (int y = 0; y < COLS; y++)
+			{
+				if (sudokuArray[x][y] == 0)
+				{
+					counter++;
+				}
+			}
+		}
+		cout << counter << endl; // TAG zeroes left
+		if (counter != 0) // If there are blank spaces
+		{
+			for (int z = 1; z < 9 + 1; z++)
+			{
+				crossHatchMarker(sudokuArray, z);
+			}
+		}
+		else if(counter == 0) // No more blank spaces
+		{
+			flag = true;
+		}
+	}
+	
+	
+}*/
+
+void playSudoku(int sudokuArray[ROWS][COLS])
 {
 	int counter = 0; // TAG
 	bool flag = false; // Prove puzzle is complete
@@ -57,17 +96,17 @@ void playSudoku(int array[ROWS][COLS])
 		{
 			for (int y = 0; y < COLS; y++)
 			{
-				if (array[x][y] == 0)
+				if (sudokuArray[x][y] == 0)
 				{
 					flag = false; // puzzle is still not complete
 					for (int x = 1; x < 9 + 1; x++)
 					{
-						crossHatchMarker(array, x);
+						crossHatchMarker(sudokuArray, x);
 						counter++;
 					}
-					
+
 				}
-				else if (x + 1 == ROWS && y + 1 == COLS && array[x][y] != 0)
+				else if (x + 1 == ROWS && y + 1 == COLS && sudokuArray[x][y] != 0)
 				{
 					flag = true; // finished
 					cout << "EXECUTED: " << counter << " times" << endl; // TAG
@@ -75,13 +114,12 @@ void playSudoku(int array[ROWS][COLS])
 			}
 		}
 	}
-	
-	
+
+
 }
 
-void crossHatchMarker(int array[ROWS][COLS], int num)
+void crossHatchMarker(int sudokuArray[ROWS][COLS], int num)
 {
-
 	int parSudoku[ROWS][COLS] = 
 	{
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -100,7 +138,7 @@ void crossHatchMarker(int array[ROWS][COLS], int num)
 	{
 		for (int y = 0; y < COLS; y++)
 		{
-			if (array[x][y] != 0)
+			if (sudokuArray[x][y] != 0)
 			{
 				parSudoku[x][y] = 1; // Marked taken spot
 			}
@@ -112,8 +150,9 @@ void crossHatchMarker(int array[ROWS][COLS], int num)
 	{
 		for (int y = 0; y < COLS; y++)
 		{
-			if (array[x][y] == num) // ROW: [x][0-9] COL: [0-9][y]
+			if (sudokuArray[x][y] == num) // ROW: [x][0-9] COL: [0-9][y]
 			{
+				cout << "NUM FOUND IN ROW: " << x << "AND COL: " << y << endl;
 				for (int z = 0; z < ROWS; z++) // Mark col 
 				{
 					parSudoku[z][y] = 1;
@@ -125,242 +164,125 @@ void crossHatchMarker(int array[ROWS][COLS], int num)
 			}
 		}
 	}
-	crossHatchPlacer(parSudoku, array, num);
-}
 
-void crossHatchPlacer(int array[ROWS][COLS], int sudokuArray[ROWS][COLS], int num)
-{
-	// Check If there are any spots for our number first
-	bool finished = false;
-	
-	int xpos;
-	int ypos;
+	cout << endl;
 
-	do
-	{
-		bool flag = false;
+		// Prints Sudoku grid
 		for (int x = 0; x < ROWS; x++)
 		{
 			for (int y = 0; y < COLS; y++)
 			{
-				if (array[x][y] == 0)
-				{
-					flag = true;
-					xpos = x;
-					ypos = y;
-					break;
-				}
-				else if (x + 1 == ROWS && y + 1 == COLS && array[x][y] != 0)
-				{
-					finished = true;
-				}
+				cout << sudokuArray[x][y] << " ";
 			}
-			if (flag == true)
-			{
-				break; // Break out of second for loop
-			}
+			cout << endl;
 		}
 
-		if (flag) // Open space is available somewhere for our number
+	// Search for open spaces to place our number
+	for (int x = 0; x < ROWS; x++)
+	{
+		for (int y = 0; y < COLS; y++)
 		{
-			// Check the subgrid we are in for duplicates
-
-			if (xpos <= 2 && ypos <= 2) // First subgrid
+			if (parSudoku[x][y] == 0)
 			{
-				bool duplicate = false; // Prove there is a duplicate
-				for (int k = 0; k < 2 + 1; k++)
-				{
-					for (int l = 0; l < 2 + 1; l++)
-					{
-						if (sudokuArray[k][l] == num) // Duplicate found
-						{
-							duplicate = true;
-						}
-					}
-				}
-
-				if (!duplicate) // If there is not a duplicate, good to go for our num to be placed there
-				{
-					sudokuArray[xpos][ypos] == num;
-					array[xpos][ypos] == 1; // Update parArray
-
-				}
-
+				crossHatchPlacer(parSudoku, sudokuArray, num, x, y); // If successful, places our number in the pos (x, y)
 			}
-			else if (xpos <= 2 && 3 <= ypos && ypos <= 5) // Second subgrid
-			{
-				bool duplicate = false; // Prove there is a duplicate
-				for (int k = 0; k < 2 + 1; k++)
-				{
-					for (int l = 3; l < 5 + 1; l++)
-					{
-						if (sudokuArray[k][l] == num) // Duplicate found
-						{
-							duplicate = true;
-						}
-					}
-				}
-
-				if (!duplicate) // If there is not a duplicate, good to go for our num to be placed there
-				{
-					sudokuArray[xpos][ypos] == num;
-					array[xpos][ypos] == 1; // Update parArray
-				}
-
-			}
-			else if (xpos <= 2 && 6 <= ypos && ypos <= 8) // Third subgrid
-			{
-				bool duplicate = false; // Prove there is a duplicate
-				for (int k = 0; k < 2 + 1; k++)
-				{
-					for (int l = 6; l < 8 + 1; l++)
-					{
-						if (sudokuArray[k][l] == num) // Duplicate found
-						{
-							duplicate = true;
-						}
-					}
-				}
-
-				if (!duplicate) // If there is not a duplicate, good to go for our num to be placed there
-				{
-					sudokuArray[xpos][ypos] == num;
-					array[xpos][ypos] == 1; // Update parArray
-				}
-
-			}
-			else if (3 <= xpos && xpos <= 5 && ypos < 2) // Fourth subgrid
-			{
-				bool duplicate = false; // Prove there is a duplicate
-				for (int k = 3; k < 5 + 1; k++)
-				{
-					for (int l = 0; l < 2 + 1; l++)
-					{
-						if (sudokuArray[k][l] == num) // Duplicate found
-						{
-							duplicate = true;
-						}
-					}
-				}
-
-				if (!duplicate) // If there is not a duplicate, good to go for our num to be placed there
-				{
-					sudokuArray[xpos][ypos] == num;
-					array[xpos][ypos] == 1; // Update parArray
-				}
-
-			}
-			else if (3 <= xpos && xpos <= 5 && 3 <= ypos && ypos <= 5) // Fifth subgrid
-			{
-				bool duplicate = false; // Prove there is a duplicate
-				for (int k = 3; k < 5 + 1; k++)
-				{
-					for (int l = 3; l < 5 + 1; l++)
-					{
-						if (sudokuArray[k][l] == num) // Duplicate found
-						{
-							duplicate = true;
-						}
-					}
-				}
-
-				if (!duplicate) // If there is not a duplicate, good to go for our num to be placed there
-				{
-					sudokuArray[xpos][ypos] == num;
-					array[xpos][ypos] == 1; // Update parArray
-				}
-
-			}
-			else if (3 <= xpos && xpos <= 5 && 6 <= ypos && ypos <= 8) // Sixth subgrid
-			{
-			bool duplicate = false; // Prove there is a duplicate
-			for (int k = 3; k < 5 + 1; k++)
-			{
-				for (int l = 6; l < 8 + 1; l++)
-				{
-					if (sudokuArray[k][l] == num) // Duplicate found
-					{
-						duplicate = true;
-					}
-				}
-			}
-
-			if (!duplicate) // If there is not a duplicate, good to go for our num to be placed there
-			{
-				sudokuArray[xpos][ypos] == num;
-				array[xpos][ypos] == 1; // Update parArray
-			}
-
-			}
-			else if (6 <= xpos && xpos <= 8 && ypos < 2) // Seventh subgrid [6-8][0-2]
-			{
-			bool duplicate = false; // Prove there is a duplicate
-			for (int k = 6; k < 8 + 1; k++)
-			{
-				for (int l = 0; l < 2 + 1; l++)
-				{
-					if (sudokuArray[k][l] == num) // Duplicate found
-					{
-						duplicate = true;
-					}
-				}
-			}
-
-			if (!duplicate) // If there is not a duplicate, good to go for our num to be placed there
-			{
-				sudokuArray[xpos][ypos] == num;
-				array[xpos][ypos] == 1; // Update parArray
-			}
-
-			}
-			else if (6 <= xpos && xpos <= 8 && 3 <= ypos <= 5) // Eigth subgrid [6-8][3-5]
-			{
-			bool duplicate = false; // Prove there is a duplicate
-			for (int k = 6; k < 8 + 1; k++)
-			{
-				for (int l = 3; l < 5 + 1; l++)
-				{
-					if (sudokuArray[k][l] == num) // Duplicate found
-					{
-						duplicate = true;
-					}
-				}
-			}
-
-			if (!duplicate) // If there is not a duplicate, good to go for our num to be placed there
-			{
-				sudokuArray[xpos][ypos] == num;
-				array[xpos][ypos] == 1; // Update parArray
-			}
-
-			}
-			else if (6 <= xpos && xpos <= 8 && 6 <= ypos <= 8) // Ninth subgrid [6-8][6-8]
-			{
-			bool duplicate = false; // Prove there is a duplicate
-			for (int k = 6; k < 8 + 1; k++)
-			{
-				for (int l = 6; l < 8 + 1; l++)
-				{
-					if (sudokuArray[k][l] == num) // Duplicate found
-					{
-						duplicate = true;
-					}
-				}
-			}
-
-			if (!duplicate) // If there is not a duplicate, good to go for our num to be placed there
-			{
-				sudokuArray[xpos][ypos] == num;
-				array[xpos][ypos] == 1; // Update parArray
-			}
-
-			}
-
 		}
-		else
-		{
-			return; // No available space
-		}
-		
-	} while (!finished);
+	}
+	
+}
+
+void crossHatchPlacer(int parArray[ROWS][COLS], int sudokuArray[ROWS][COLS], int num, int xpos, int ypos)
+{
+						// Check the subgrid we are in for duplicates
+	
+						if (0 <= xpos && xpos <= 2 && 0 <= ypos && ypos <= 2) // First subgrid [0-2][0-2]
+						{
+							subGridAnalyzer(parArray, sudokuArray, num, 0, 2, 0, 2, xpos, ypos);
+						}
+						else if (0 <= xpos && xpos <= 2 && 3 <= ypos && ypos <= 5) // Second subgrid [0-2][3-5]
+						{
+							subGridAnalyzer(parArray, sudokuArray, num, 0, 2, 3, 5, xpos, ypos);
+						}
+						else if (0 <= xpos && xpos <= 2 && 6 <= ypos && ypos <= 8) // Third subgrid [0-2][6-8]
+						{
+							subGridAnalyzer(parArray, sudokuArray, num, 0, 2, 6, 8, xpos, ypos);
+						}
+						else if (3 <= xpos && xpos <= 5 && 0 <= ypos && ypos <= 2) // Fourth subgrid [3-5][0-2]
+						{
+							subGridAnalyzer(parArray, sudokuArray, num, 3, 5, 0, 2, xpos, ypos);
+						}
+						else if (3 <= xpos && xpos <= 5 && 3 <= ypos && ypos <= 5) // Fifth subgrid [3-5][3-5]
+						{
+							subGridAnalyzer(parArray, sudokuArray, num, 3, 5, 3, 5, xpos, ypos);
+						}
+						else if (3 <= xpos && xpos <= 5 && 6 <= ypos && ypos <= 8) // Sixth subgrid [3-5][6-8]
+						{
+							subGridAnalyzer(parArray, sudokuArray, num, 3, 5, 6, 8, xpos, ypos);
+						}
+						else if (6 <= xpos && xpos <= 8 && 0 <= ypos && ypos <= 2) // Seventh subgrid [6-8][0-2]
+						{
+							subGridAnalyzer(parArray, sudokuArray, num, 6, 8, 0, 2, xpos, ypos);
+						}
+						else if (6 <= xpos && xpos <= 8 && 3 <= ypos && ypos <= 5) // Eigth subgrid [6-8][3-5]
+						{
+							subGridAnalyzer(parArray, sudokuArray, num, 6, 8, 3, 5, xpos, ypos);
+						}
+						else if (6 <= xpos && xpos <= 8 && 6 <= ypos && ypos <= 8) // Ninth subgrid [6-8][6-8]
+						{
+							subGridAnalyzer(parArray, sudokuArray, num, 6, 8, 6, 8, xpos, ypos);
+						}
+}
+
+void subGridAnalyzer(int parArray[ROWS][COLS], int sudokuArray[ROWS][COLS], int num, int xLower, int xUpper, int yLower, int yUpper, int xpos, int ypos)
+{
+	//cout << "In subgrid: [" << xLower << "-" << xUpper << "][" << yLower << "-" << yUpper << "]" << endl << endl;
+						int openSpace = 0; // Must be 1 for a number to be placed
+						bool duplicate = false; // Prove there is a duplicate
+						for (int k = xLower; k < (xUpper + 1); k++)
+						{
+							for (int l = yLower; l < (yUpper + 1); l++)
+							{
+								if (parArray[k][l] == 0)
+								{
+									openSpace += 1;
+									//cout << "POTENTIAL OPEN SPACE!" << endl;
+									//cout << "PAR Sudoku box: " << "k: " << k << " l: " << l << " holds: " << sudokuArray[k][l] << endl;
+								}
+								//cout << "Sudoku box: " << "k: " << k << " l: " << l << " holds: " << sudokuArray[k][l] << endl;
+								if (sudokuArray[k][l] == num) // Duplicate found
+								{
+									duplicate = true;
+								}
+
+								//cout << parArray[k][l] << " "; // TAG
+
+							}
+							//cout << endl; // TAG
+						}
+
+						if (duplicate) // TAG
+						{
+							// cout << "Duplicate found!" << endl;
+						}
+
+						if (!duplicate && parArray[xpos][ypos] != 1 && openSpace == 1) // If there is not a duplicate, good to go for our num to be placed there
+						{
+							placeNum(parArray, sudokuArray, num, xpos, ypos);
+							cout << "****" << endl;
+						}
+
+						
+}
+
+void placeNum(int parArray[ROWS][COLS], int sudokuArray[ROWS][COLS], int num, int xpos, int ypos)
+{
+	sudokuArray[xpos][ypos] = num;
+
+	// Update parArray
+
+
+
+
+	cout << "Num: " << num << " placed @ " << "(" << xpos << ", " << ypos << ")" << endl;
+//	cout << "PAR ARRAY @ (" << xpos << ", " << ypos << ") is: " << parArray[xpos][ypos]<< endl;
 }
